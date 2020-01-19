5de493da261788e22d46b74c0c66f738488e1604
Max Luebbers <luebbersma@gmail.com>
2020-01-19 10:54:44 -0500

    (FIX) Renamed repo.

--- getCommits.js
+++ getCommits.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
         console.log("ready");
-        $.get("https://api.github.com/repos/MLuebbers/pensive/commits?path=./index.html", function(data, status) {
+        $.get("https://api.github.com/repos/MLuebbers/pensieve/commits?path=./index.html", function(data, status) {
             console.log(data[0].commit.committer.date);
             console.log(data.length);
             $("#last-commit").append(data[0].commit.committer.date);
--- getFiles.js
+++ getFiles.js
@@ -2,7 +2,7 @@ let buttonClick = function() {
     let i = 1;
     return function () {
         console.log(i);
-        $.get("https://api.github.com/repos/MLuebbers/pensive/commits?path=./index.html", function(data, status) {
+        $.get("https://api.github.com/repos/MLuebbers/pensieve/commits?path=./index.html", function(data, status) {
             console.log(data);
             if(i < data.length) {
                 $.ajax({
@@ -11,7 +11,7 @@ let buttonClick = function() {
                     },
                     data: "data",
                     method: "GET",
-                    url: `https://api.github.com/repos/MLuebbers/pensive/contents/index.html?ref=${data[i].sha}`,
+                    url: `https://api.github.com/repos/MLuebbers/pensieve/contents/index.html?ref=${data[i].sha}`,
             
                     success: function(response) {
                         console.log(response);
